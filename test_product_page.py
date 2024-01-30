33148235cff24fe5eb67f5505db127d6980a9a6e
VeraQA <kashirina.vera@gmail.com>
2024-01-30 18:21:19 +0300

    update format

--- test_product_page.py
+++ test_product_page.py
@@ -1,14 +1,14 @@
 from .pages.base_page import BasePage
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 import pytest
 import time
 
 
-
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(autouse=True)
-    def setup(self,browser):
+    def setup(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
         page = LoginPage(browser, link)
         page.open()
@@ -18,20 +18,22 @@ class TestUserAddToBasketFromProductPage():
         page.should_be_authorized_user()
 
     @pytest.mark.need_review
-    def test_user_can_add_product_to_basket(self,browser):
+    def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
-        page = ProductPage(browser,link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page = ProductPage(browser,
+                           link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()
         page.add_product()
         page.should_have_right_name()
         page.should_have_one_price_basket_and_price()
 
-    def test_user_cant_see_success_message(self,browser):
+    def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
         page = ProductPage(browser,
-                        link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+                           link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()
         ProductPage.should_not_be_success_message(page)
+
 
 # @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -43,7 +45,8 @@ class TestUserAddToBasketFromProductPage():
 #                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail) ,
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-@pytest.mark.parametrize('link',["http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"])
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"])
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, link):
     page = BasePage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()
@@ -77,6 +80,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     ProductPage.should_dissappeare(page)
 
 
+@pytest.mark.need_review
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
     page = ProductPage(browser, link)
@@ -92,9 +96,10 @@ def test_guest_can_go_to_login_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
-    page = BasePage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = BasketPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.open_basket_page()
     BasketPage.should_be_empty_basket(page)
